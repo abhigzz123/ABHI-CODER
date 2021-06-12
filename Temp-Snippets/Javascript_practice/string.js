4d04b3d981761de1f36acf0c6a3dc5fd8fe570ac
abhigzz123 <instant.knowledge.9@gmail.com>
2021-06-12 19:52:58 +0530

    String-Commit

---
+++ Temp-Snippets/Javascript_practice/string.js
@@ -0,0 +1,16 @@
+//STRING PRACTICE
+x = 'Name_xyz_abc' ;
+console.log(x[0]) ;
+console.log(x.length) ;
+
+// STRING METHODS
+
+console.log(x.toUpperCase()) ;
+console.log(x.toLowerCase()) ;
+console.log(x.indexOf('_') ) ;
+console.log(x.lastIndexOf('_')) ;
+
+//slicing
+console.log(x.slice(0, 2)) ;
+console.log(x.substr(0 ,5)) ;
+console.log(x.replace('Name' , 'NO_NAME')) ;
